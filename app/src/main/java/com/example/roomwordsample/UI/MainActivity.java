c80c9131672e01e7ca7aace80958348b68845792
Shaddhiix <leonard.alexandre17@gmail.com>
2022-06-29 11:34:14 +0200

    initial

--- app/src/main/java/com/example/roomwordsample/UI/MainActivity.java
+++ app/src/main/java/com/example/roomwordsample/UI/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.roomwordsample;
+package com.example.roomwordsample.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.roomwordsample.R;
 
 public class MainActivity extends AppCompatActivity {
 
